f351b25d8bf6bf24808c4bd1071e594d18de263b
Jean Finas <jean@finas.io>
2019-07-30 10:21:39 +0200

    correct typo in application name

--- src/main/java/de/klosebrothers/jfinas/bowlingkata/BowlingKataApplication.java
+++ src/main/java/de/klosebrothers/jfinas/bowlingkata/BowlingKataApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BowlingkataApplication {
+public class BowlingKataApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(BowlingkataApplication.class, args);
+        SpringApplication.run(BowlingKataApplication.class, args);
     }
 
 }
--- src/test/java/de/klosebrothers/jfinas/bowlingkata/BowlingKataApplicationTests.java
+++ src/test/java/de/klosebrothers/jfinas/bowlingkata/BowlingKataApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BowlingkataApplicationTests {
+public class BowlingKataApplicationTests {
 
     @Test
     public void contextLoads() {
