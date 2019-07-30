bfebe408418eeb2f01eb9f47415b847f09a420e7
jfinas <32193107+jfinas@users.noreply.github.com>
2019-07-30 14:21:41 +0200

    Implement a representation fo a whole Bowling Game, with a method to calculate the score of the game

--- src/main/java/de/klosebrothers/jfinas/bowlingkata/BowlingGame.java
+++ src/main/java/de/klosebrothers/jfinas/bowlingkata/BowlingGame.java
@@ -1,4 +1,27 @@
 package de.klosebrothers.jfinas.bowlingkata;
 
 public class BowlingGame {
+    BowlingFrame[] bowlingFrames;
+
+    public BowlingGame(BowlingFrame[] bowlingFrames) {
+        this.bowlingFrames = bowlingFrames;
+    }
+
+    /**
+     * Calculates the Score of a Line of Bowling, according to the rules given in the Kata
+     *
+     * @return the calculated score
+     */
+    public int calculateScore() {
+        int score = 0;
+        for (int i = 0; i < 10; i++) {
+            BowlingFrame currentFrame = bowlingFrames[i];
+            score += currentFrame.getScore();
+            if (currentFrame.isStrike())
+                score += bowlingFrames[i + 1].getScore() + bowlingFrames[i + 2].getScore();
+            if (currentFrame.isSpare())
+                score += bowlingFrames[i + 1].getFirstThrowValue();
+        }
+        return score;
+    }
 }
---
+++ src/test/java/de/klosebrothers/jfinas/bowlingkata/BowlingGameTest.java
@@ -0,0 +1,244 @@
+package de.klosebrothers.jfinas.bowlingkata;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BowlingGameTest {
+    @Test
+    public void calculateScoreShouldReturnZeroGivenOnliMissesShouldReturnZero() {
+        BowlingFrame[] testFrames = {
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+        };
+
+        BowlingGame testGame = new BowlingGame(testFrames);
+        int expected = 0;
+        int actual = testGame.calculateScore();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void calculateScoreShouldReturnCorrectScoreGivenOneNormalFrame() {
+        BowlingFrame[] testFrames = {
+                new BowlingFrame("52".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+        };
+
+        BowlingGame testGame = new BowlingGame(testFrames);
+        int expected = 7;
+        int actual = testGame.calculateScore();
+        Assertions.assertEquals(expected, actual);
+    }
+
+
+    @Test
+    public void calculateScoreShouldReturnCorrectValueGivenStrike() {
+        BowlingFrame[] testFrames = {
+                new BowlingFrame("X ".toCharArray()),
+                new BowlingFrame("52".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+        };
+        BowlingGame testGame = new BowlingGame(testFrames);
+        int expected = 17 + 7;
+        int actual = testGame.calculateScore();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void calculateScoreShouldReturnCorrectValueGivenSpare() {
+        BowlingFrame[] testFrames = {
+                new BowlingFrame("5/ ".toCharArray()),
+                new BowlingFrame("52".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+        };
+        BowlingGame testGame = new BowlingGame(testFrames);
+        int expected = 15 + 7;
+        int actual = testGame.calculateScore();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void calculateScoreShouldReturnCorrectValueNormalFrameAtEnd() {
+        BowlingFrame[] testFrames = {
+                new BowlingFrame("X ".toCharArray()),
+                new BowlingFrame("52".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("53".toCharArray()),
+        };
+        BowlingGame testGame = new BowlingGame(testFrames);
+        int expected = 17 + 7 + 8;
+        int actual = testGame.calculateScore();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void calculateScoreShouldReturnCorrectValueGivenStrikeAtEnd() {
+        BowlingFrame[] testFrames = {
+                new BowlingFrame("X ".toCharArray()),
+                new BowlingFrame("52".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("X ".toCharArray()),
+                new BowlingFrame("53".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+        };
+        BowlingGame testGame = new BowlingGame(testFrames);
+        int expected = 17 + 7 + 18;
+        int actual = testGame.calculateScore();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void calculateScoreShouldReturnCorrectValueGivenSpareAtEnd() {
+        BowlingFrame[] testFrames = {
+                new BowlingFrame("X ".toCharArray()),
+                new BowlingFrame("52".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("5/ ".toCharArray()),
+                new BowlingFrame("5 ".toCharArray()),
+                new BowlingFrame("3 ".toCharArray()),
+        };
+        BowlingGame testGame = new BowlingGame(testFrames);
+        int expected = 17 + 7 + 15;
+        int actual = testGame.calculateScore();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void calculateScoreShouldReturnCorrectValueGivenStrikeAtEndnNormalFrameAsBonus() {
+        BowlingFrame[] testFrames = {
+                new BowlingFrame("X ".toCharArray()),
+                new BowlingFrame("52".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("--".toCharArray()),
+                new BowlingFrame("X ".toCharArray()),
+                new BowlingFrame("53".toCharArray()),
+                new BowlingFrame("  ".toCharArray()),
+        };
+        BowlingGame testGame = new BowlingGame(testFrames);
+        int expected = 17 + 7 + 10 + 8;
+        int actual = testGame.calculateScore();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    // Tests provided by the kata
+    @Test
+    public void calculateScoreShouldReturnCorrectValueGivenOnlyStrikes() {
+        BowlingFrame[] testFrames = {
+                new BowlingFrame("X ".toCharArray()),
+                new BowlingFrame("X ".toCharArray()),
+                new BowlingFrame("X ".toCharArray()),
+                new BowlingFrame("X ".toCharArray()),
+                new BowlingFrame("X ".toCharArray()),
+                new BowlingFrame("X ".toCharArray()),
+                new BowlingFrame("X ".toCharArray()),
+                new BowlingFrame("X ".toCharArray()),
+                new BowlingFrame("X ".toCharArray()),
+                new BowlingFrame("X ".toCharArray()),
+                new BowlingFrame("X ".toCharArray()),
+                new BowlingFrame("X ".toCharArray()),
+        };
+        BowlingGame testGame = new BowlingGame(testFrames);
+        int expected = 10 * 30;
+        int actual = testGame.calculateScore();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void calculateScoreShouldReturnCorrectValueGivenNormalFrames() {
+        BowlingFrame[] testFrames = {
+                new BowlingFrame("9-".toCharArray()),
+                new BowlingFrame("9-".toCharArray()),
+                new BowlingFrame("9-".toCharArray()),
+                new BowlingFrame("9-".toCharArray()),
+                new BowlingFrame("9-".toCharArray()),
+                new BowlingFrame("9-".toCharArray()),
+                new BowlingFrame("9-".toCharArray()),
+                new BowlingFrame("9-".toCharArray()),
+                new BowlingFrame("9-".toCharArray()),
+                new BowlingFrame("9-".toCharArray()),
+                new BowlingFrame("9-".toCharArray()),
+                new BowlingFrame("9-".toCharArray()),
+        };
+        BowlingGame testGame = new BowlingGame(testFrames);
+        int expected = 9 * 10;
+        int actual = testGame.calculateScore();
+        Assertions.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void calculateScoreShouldReturnCorrectValueGivenOnlySpares() {
+        BowlingFrame[] testFrames = {
+                new BowlingFrame("5/".toCharArray()),
+                new BowlingFrame("5/".toCharArray()),
+                new BowlingFrame("5/".toCharArray()),
+                new BowlingFrame("5/".toCharArray()),
+                new BowlingFrame("5/".toCharArray()),
+                new BowlingFrame("5/".toCharArray()),
+                new BowlingFrame("5/".toCharArray()),
+                new BowlingFrame("5/".toCharArray()),
+                new BowlingFrame("5/".toCharArray()),
+                new BowlingFrame("5/".toCharArray()),
+                new BowlingFrame("5 ".toCharArray()),
+                new BowlingFrame("  ".toCharArray()),
+        };
+        BowlingGame testGame = new BowlingGame(testFrames);
+        int expected = 10 * 15;
+        int actual = testGame.calculateScore();
+        Assertions.assertEquals(expected, actual);
+    }
+}
