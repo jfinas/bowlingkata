148fcc63c9474bef01982223b52e399f5d5444c3
Jean Finas <jean@finas.io>
2019-07-30 10:06:54 +0200

    add initial project setup, generated by Spring Initializr

---
+++ src/main/java/de/klosebrothers/jfinas/bowlingkata/BowlingkataApplication.java
@@ -0,0 +1,13 @@
+package de.klosebrothers.jfinas.bowlingkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BowlingkataApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BowlingkataApplication.class, args);
+    }
+
+}
